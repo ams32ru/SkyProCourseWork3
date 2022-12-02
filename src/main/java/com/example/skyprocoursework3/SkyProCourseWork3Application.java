b207103ef4a2a07eb80845dc6bcaf374e284c82d
ams32ru <ams32ru@mail.ru>
2022-12-02 20:12:15 +0300

    master

---
+++ src/main/java/com/example/skyprocoursework3/SkyProCourseWork3Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCourseWork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProCourseWork3Application.class, args);
+    }
+
+}
